a869507764e6c186189f23da639ae52462223d4a
gnarzulllayev <gnarzulllayev2000@gmail.com>
2023-03-11 18:04:59 +0500

    update

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,2 +1,2 @@
 // export const apiUrl = "http://localhost:4000/api/v1/"
-export const apiUrl = "https://crm-api.tezzkor.com/api/v1/"
+export const apiUrl = "https://crm-api.itpark-qarshi.uz/api/v1/"
